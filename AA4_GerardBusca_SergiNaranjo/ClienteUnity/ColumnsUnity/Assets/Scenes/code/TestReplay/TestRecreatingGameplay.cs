f6da693ff766bef8b20718782a8e8732a7a4c314
Monke <gerard.busca@estudiant.enti.cat>
2026-01-24 19:28:24 +0100

    Added replay mode
    Still in development

---
+++ AA4_GerardBusca_SergiNaranjo/ClienteUnity/ColumnsUnity/Assets/Scenes/code/TestReplay/TestRecreatingGameplay.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TestRecreatingGameplay : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    void Update()
+    {
+        // Por ejemplo, spawneamos al presionar la barra espaciadora
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnLine();
+        }
+    }
+
+    void SpawnLine()
+    {
+        int randomNumber = Random.Range(-4, 4);
+        // Puedes elegir la posición donde quieres spawnearlo
+        Vector3 spawnPosition = new Vector3(randomNumber, 6.14f, 0f);
+
+        // Instanciamos el prefab
+        GameObject newLine = Instantiate(linePrefab, spawnPosition, Quaternion.identity);
+
+        // Opcional: cambiar nombre del objeto instanciado
+        newLine.name = "LineInstance";
+    }
+}
