c8cb9db6db0c847f62d6209e439f3e4388ec409a
MaquinaVirtualClase <gerard.busca@estudiant.enti.cat>
2026-01-30 12:19:50 +0100

    Added lists

--- AA4_GerardBusca_SergiNaranjo/Servidor/routes/server.js
+++ AA4_GerardBusca_SergiNaranjo/Servidor/routes/server.js
@@ -7,43 +7,86 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "public"))); 
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// Manejo de salas
 const rooms = {};
 
-io.on("connection", (socket) => {
-    console.log("Usuario conectado");
+function updateRoomState(roomName) {
+  const count = rooms[roomName]?.length || 0;
 
-    socket.on("joinRoom", ({ roomName, message }) => {
-        if (!rooms[roomName]) rooms[roomName] = [];
-        rooms[roomName].push(socket.id);
-        socket.join(roomName);
+  if (count === 0) {
+    io.to(roomName).emit("roomPaused", {});
+    console.log(`Sala ${roomName} pausada`);
+  } else {
+    io.to(roomName).emit("roomResumed", {});
+    console.log(`Sala ${roomName} reanudada`);
+  }
+}
+
+io.on("connection", socket => {
+  console.log("Usuario conectado:", socket.id);
+
+  socket.on("joinRoom", ({ roomName }) => {
+    if (!rooms[roomName]) rooms[roomName] = [];
+
+    rooms[roomName].push(socket.id);
+    socket.join(roomName);
+
+    io.to(roomName).emit("roomUpdate", {
+      players: rooms[roomName].length
+    });
+
+    updateRoomState(roomName);
+  });
+
+  socket.on("leaveRoom", ({ roomName }) => {
+    if (!rooms[roomName]) return;
+
+    rooms[roomName] = rooms[roomName].filter(id => id !== socket.id);
+    socket.leave(roomName);
+
+    io.to(roomName).emit("roomUpdate", {
+      players: rooms[roomName].length
+    });
+
+    updateRoomState(roomName);
+
+    if (rooms[roomName].length === 0) {
+      delete rooms[roomName];
+    }
+  });
+
+  socket.on("playerMove", direction => {
+    socket.broadcast.emit("unityMove", direction);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Usuario desconectado:", socket.id);
+
+    for (const roomName in rooms) {
+      if (rooms[roomName].includes(socket.id)) {
+        rooms[roomName] = rooms[roomName].filter(id => id !== socket.id);
 
         io.to(roomName).emit("roomUpdate", {
-            players: rooms[roomName].length,
-            message: message || `Bienvenido a ${roomName}`
+          players: rooms[roomName].length
         });
-    });
 
-    socket.on("disconnect", () => {
-        for (const room in rooms) {
-            rooms[room] = rooms[room].filter(id => id !== socket.id);
-            io.to(room).emit("roomUpdate", {
-                players: rooms[room].length,
-                message: `Jugador se ha desconectado de ${room}`
-            });
+        updateRoomState(roomName);
+
+        if (rooms[roomName].length === 0) {
+          delete rooms[roomName];
         }
-    });
+      }
+    }
+  });
 });
 
-server.listen(app.get("port"), "0.0.0.0", () => {
-    const ip = ipHelper.address();
-    const port = app.get("port");
-    console.log("Servidor arrancado en la url: http://" + ip + ":" + port + "/");
+server.listen(PORT, () => {
+  console.log(`Servidor activo en http://localhost:${PORT}`);
 });
